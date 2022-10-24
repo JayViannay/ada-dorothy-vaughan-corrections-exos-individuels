a0cef2727329303055dbb587a92bbb81e40be5ea
JennyViannay <jenny.viannay75@gmail.com>
2022-10-24 13:35:34 +0200

    step 1 ok

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('👋 Hello from main.js');
+
+// --- Step 1 ---
+
+// Créer une fonction qui demande à l’utilisateur à l’aide d’un prompteur, un nombre. 
+function askNumber () {
+    return parseInt(prompt('Guess a number'));
+}
+
+// Stocker sa réponse dans une variable de type adéquate nommée givenNumber.
+let givenNumber = askNumber();
+console.log(givenNumber);
